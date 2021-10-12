98bd89151f54fd37ade3442ea8d7fdbc3b814d54
Kęstutis Bivainis <bivainis@gmail.com>
2021-10-13 00:09:50 +0300

    added board.h added spaces at the end board goes back to default clock

--- LPC800_mini_overcloking/src/main.c
+++ LPC800_mini_overcloking/src/main.c
@@ -10,9 +10,7 @@
 #include "nokia6100.h"
 #include "xprintf.h"
 #include "delay.h"
-
-const uint32_t OscRateIn = 12000000;
-const uint32_t ExtRateIn = 0;
+#include "board.h"
 
 typedef struct {
   uint8_t MSEL;
@@ -51,9 +49,9 @@ MODE modes[] = {
   {5,1,1},// 72 MHz overclocked
 };
 
-volatile uint32_t counter=0;
+volatile uint32_t counter = 0;
 
-void MRT_IRQHandler(void) {
+void MRT0_IRQHandler(void) {
 
   if (Chip_MRT_IntPending(LPC_MRT_CH0)) {
     Chip_MRT_IntClear(LPC_MRT_CH0);
@@ -77,33 +75,34 @@ uint32_t xor128(void) {
   return w = w ^ (w >> 19) ^ (t ^ (t >> 8));
 }
 
-void display_info(uint32_t clk,uint8_t pos,uint32_t time){
+void display_info(uint32_t clk, uint8_t pos, uint32_t time){
 
   char buf[24];
   uint32_t after_decimal;
 
   // clear screen
-  LCDRect(START_X,START_Y,END_X,END_Y,BLACK);
+  LCDRect(START_X, START_Y, END_X, END_Y, BLACK);
 
   LCDPutStr("LPC800 Mini-Kit", 120, 16);
-  LCDPutStr("from 2 to 72 Mhz", 110,14);
-  xsprintf(buf,"MSEL:%d PSEL:%d DIV:%d",modes[pos].MSEL,modes[pos].PSEL,modes[pos].DIV);
-  LCDPutStr(buf,80,4);
-  after_decimal=clk-(clk/1000)*1000;
-  xsprintf(buf,"Clock: %d.%d MHz",clk/1000,after_decimal/100);
-  LCDPutStr(buf,65,4);
-  if(time) {
-    xsprintf(buf,"Time: %d ms",counter);
-    LCDPutStr(buf,20,4);
+  LCDPutStr("from 2 to 72 Mhz", 110, 14);
+  xsprintf(buf, "MSEL:%d PSEL:%d DIV:%d", modes[pos].MSEL, modes[pos].PSEL, modes[pos].DIV);
+  LCDPutStr(buf, 80, 4);
+  after_decimal = clk - (clk / 1000) * 1000;
+  xsprintf(buf, "Clock: %d.%d MHz", clk / 1000, after_decimal / 100);
+  LCDPutStr(buf, 65, 4);  
+ 
+  if (time) {
+    xsprintf(buf, "Time: %d ms", counter);
+    LCDPutStr(buf, 20, 4);
   }
   delay_ms(2000);
   // clear screen
-  LCDRect(START_X,START_Y,END_X,END_Y,BLACK);
+  LCDRect(START_X, START_Y, END_X, END_Y, BLACK);
 }
 
 int32_t main(void) {
 
-  uint32_t i,j,clk;
+  uint32_t i, j, clk;
 
   uint32_t p1x;
   uint32_t p1y;
@@ -117,9 +116,11 @@ int32_t main(void) {
   Chip_GPIO_Init(LPC_GPIO_PORT);
   Chip_SYSCTL_PeriphReset(RESET_GPIO);
 
+  Board_Init();
+  
 // init LCD reset pin
   Chip_GPIO_SetPinState(LPC_GPIO_PORT, 0, RESET_PIN, true);
-  Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT,0,RESET_PIN);
+  Chip_GPIO_SetPinDIROutput(LPC_GPIO_PORT, 0, RESET_PIN);
 
   // init SWM
   Chip_SWM_Init();
@@ -129,29 +130,30 @@ int32_t main(void) {
   Chip_SWM_DisableFixedPin(SWM_FIXED_ACMP_I1);//PIO0_0
   Chip_SWM_MovablePinAssign(SWM_SPI0_SCK_IO,CLK_PIN);
   Chip_SWM_MovablePinAssign(SWM_SPI0_MOSI_IO,MOSI_PIN);
+  Chip_SWM_Deinit();
 
 // init SPI0 at SystemCoreClock speed
   Chip_SPI_Init(LPC_SPI0);
-  Chip_SPI_ConfigureSPI(LPC_SPI0,SPI_MODE_MASTER|
-                                 SPI_CLOCK_CPHA0_CPOL0|
-                                 SPI_DATA_MSB_FIRST|
-                                 SPI_SSEL_ACTIVE_LO);
+  Chip_SPI_ConfigureSPI(LPC_SPI0, SPI_MODE_MASTER |
+                                  SPI_CLOCK_CPHA0_CPOL0 |
+                                  SPI_DATA_MSB_FIRST |
+                                  SPI_SSEL_ACTIVE_LO);
   //Chip_SPIM_SetClockRate(LPC_SPI0,SystemCoreClock);
-  LPC_SPI0->DIV=0;
+  LPC_SPI0->DIV = 0;
   Chip_SPI_Enable(LPC_SPI0);
 
   // init MRT
   Chip_MRT_Init();
 // Channel0 for miliseconds delay
-  Chip_MRT_SetMode(LPC_MRT_CH0,MRT_MODE_REPEAT);
-  Chip_MRT_SetInterval(LPC_MRT_CH0, SystemCoreClock/1000 | MRT_INTVAL_LOAD);
+  Chip_MRT_SetMode(LPC_MRT_CH0, MRT_MODE_REPEAT);
+  Chip_MRT_SetInterval(LPC_MRT_CH0, SystemCoreClock / 1000 | MRT_INTVAL_LOAD);
   Chip_MRT_SetEnabled(LPC_MRT_CH0);
   NVIC_EnableIRQ(MRT_IRQn);
 
 // init LCD
   LCDInit();
   // clear screen
-  LCDRect(START_X,START_Y,END_X,END_Y,BLACK);
+  LCDRect(START_X, START_Y, END_X, END_Y, BLACK);
 
   LCDPutStr("LPC800 Mini-Kit", 120, 16);
   LCDPutStr("ARM Cortex M0+", 110, 20);
@@ -164,28 +166,28 @@ int32_t main(void) {
   LCDPutStr("More at:", 10,4);
   LCDPutStr("kbiva.wordpress.com", 1,4);
   delay_ms(5000);
-
+  
   // go from 2 to 72 Mhz
-  for(j=0;j<sizeof(modes)/sizeof(MODE);j++) {
+  for (j = 0; j < sizeof(modes) / sizeof(MODE); j++) {
 
     // setup new clock speed using PLL
     // power down PLL when changing MSEL and PSEL, chapter 4.7.4.3.3 in LPC81x User manual (UM10601)
     Chip_Clock_SetMainClockSource(SYSCTL_MAINCLKSRC_IRC);
     Chip_SYSCTL_PowerDown(SYSCTL_SLPWAKE_SYSPLL_PD);
-    Chip_Clock_SetupSystemPLL(modes[j].MSEL,modes[j].PSEL);
+    Chip_Clock_SetupSystemPLL(modes[j].MSEL, modes[j].PSEL);
     Chip_SYSCTL_PowerUp(SYSCTL_SLPWAKE_SYSPLL_PD);
-    while(!Chip_Clock_IsSystemPLLLocked()){};
+    while (!Chip_Clock_IsSystemPLLLocked()) {};
     Chip_Clock_SetSysClockDiv(modes[j].DIV);
     Chip_Clock_SetMainClockSource(SYSCTL_MAINCLKSRC_PLLOUT);
 
     SystemCoreClockUpdate();
 
-    clk=SystemCoreClock/1000;
+    clk = SystemCoreClock / 1000;
 
     // Set new millisecond interval because clock speed changed
     Chip_MRT_SetInterval(LPC_MRT_CH0, clk | MRT_INTVAL_LOAD);
 
-    display_info(clk,j,0);
+    display_info(clk, j, 0);
 
     // reset pseudo-random number generator
     x = 123456789;
@@ -194,7 +196,9 @@ int32_t main(void) {
     w = 88675123;
 
     // reset milliseconds counter
-    counter=0;
+    counter = 0;
+
+    Board_LED_Set(0, true);
 
     // do 100 filled rectangles
     for (i = 0; i < 100; i++) {
@@ -203,10 +207,16 @@ int32_t main(void) {
       p2x = xor128() % MAX_X;
       p2y = xor128() % MAX_Y;
       pcol = xor128() % 256;
-      LCDRect(p1x,p1y,p2x,p2y, pcol);
+      LCDRect(p1x, p1y, p2x, p2y, pcol);
     }
+    
+    Board_LED_Set(0, false);
+    
     // display results
-    display_info(clk,j,counter);
+    display_info(clk, j, counter);
   }
+  Chip_Clock_SetMainClockSource(SYSCTL_MAINCLKSRC_IRC);
+    
   while(1){};
+  
 }
